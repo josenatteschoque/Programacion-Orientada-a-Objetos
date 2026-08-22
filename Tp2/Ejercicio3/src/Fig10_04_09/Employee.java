5d7e9b9fd83dd97ca9cbbeed3b43f8846ed12bf6
josenatteschoque <josenatteschoque@gmail.com>
2026-08-21 23:56:37 -0300

    Ejercicio Finalizado!

--- Tp2/Ejercicio3/src/Fig10_04_09/Employee.java
+++ Tp2/Ejercicio3/src/Fig10_04_09/Employee.java
@@ -4,101 +4,92 @@ package Fig10_04_09;
 // Employee abstract superclass.
 import java.time.LocalDate;
 
-public abstract class Employee 
-{
-   private String firstName;
-   private String lastName;
-   private String socialSecurityNumber;
-   private LocalDate birthDate;
-   private static final int additional = 100;
+public abstract class Employee {
+	private String firstName;
+	private String lastName;
+	private String socialSecurityNumber;
+	private LocalDate birthDate;
+	private static final int additional = 300;
 
-   // three-argument constructor
-   public Employee( String first, String last, String ssn)
-   {
-      firstName = first;
-      lastName = last;
-      socialSecurityNumber = ssn;
-   } // end three-argument Employee constructor
+	// three-argument constructor
+	public Employee(String first, String last, String ssn) {
+		firstName = first;
+		lastName = last;
+		socialSecurityNumber = ssn;
+	} // end three-argument Employee constructor
 
-   public LocalDate getBirthDate() {
-	return birthDate;
-}
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
 
-   public void setBirthDate(LocalDate birthDate) {
-	this.birthDate = birthDate;
-   }
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
 
-   public static int getAdditional() {
- 		return additional;
- 	}
-   
-   // set first name
-   public void setFirstName( String first )
-   {
-      firstName = first;
-   } // end method setFirstName
+	public static int getAdditional() {
+		return additional;
+	}
 
-   // return first name
-   public String getFirstName()
-   {
-      return firstName;
-   } // end method getFirstName
+	// set first name
+	public void setFirstName(String first) {
+		firstName = first;
+	} // end method setFirstName
 
-   // set last name
-   public void setLastName( String last )
-   {
-      lastName = last;
-   } // end method setLastName
+	// return first name
+	public String getFirstName() {
+		return firstName;
+	} // end method getFirstName
 
-   // return last name
-   public String getLastName()
-   {
-      return lastName;
-   } // end method getLastName
+	// set last name
+	public void setLastName(String last) {
+		lastName = last;
+	} // end method setLastName
 
-   // set social security number
-   public void setSocialSecurityNumber( String ssn )
-   {
-      socialSecurityNumber = ssn; // should validate
-   } // end method setSocialSecurityNumber
+	// return last name
+	public String getLastName() {
+		return lastName;
+	} // end method getLastName
 
-   // return social security number
-   public String getSocialSecurityNumber()
-   {
-      return socialSecurityNumber;
-   } // end method getSocialSecurityNumber
+	// set social security number
+	public void setSocialSecurityNumber(String ssn) {
+		socialSecurityNumber = ssn; // should validate
+	} // end method setSocialSecurityNumber
 
-   // return String representation of Employee object
-   public String toString()
-   {
-      return String.format( "%s %s\nsocial security number: %s", 
-         getFirstName(), getLastName(), getSocialSecurityNumber() );
-   } // end method toString
+	// return social security number
+	public String getSocialSecurityNumber() {
+		return socialSecurityNumber;
+	} // end method getSocialSecurityNumber
 
-   // abstract method overridden by subclasses
-   public abstract double earnings(); // no implementation here
-   
-   public double salary(LocalDate date) {
-	   if(birthDate != null && date.getMonthValue() == birthDate.getMonthValue()) {
-		   return earnings() + getAdditional();
-	   }
-	   return earnings();
-	   
-   }
+	// return String representation of Employee object
+	public String toString() {
+		return String.format("%s %s\nsocial security number: %s", getFirstName(), getLastName(),
+				getSocialSecurityNumber());
+	} // end method toString
+
+	// abstract method overridden by subclasses
+	public abstract double earnings(); // no implementation here
+
+	// Metodo que Calcula el sueldo de un empleado y si su mes de cumpleaños
+	// conincide con el de la liquidacion se le otorga un adiccional a su sueldo
+	public double salary(LocalDate date) {
+		if (birthDate != null && date.getMonthValue() == birthDate.getMonthValue()) {
+			System.out.println("Se le hizo el adiccional al sueldo!");
+			return earnings() + getAdditional();
+		}
+		return earnings();
+
+	}
 } // end abstract class Employee
 
-
 /**************************************************************************
- * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and               *
- * Pearson Education, Inc. All Rights Reserved.                           *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
+ * (C) Copyright 1992-2007 by Deitel & Associates, Inc. and * Pearson Education,
+ * Inc. All Rights Reserved. * * DISCLAIMER: The authors and publisher of this
+ * book have used their * best efforts in preparing the book. These efforts
+ * include the * development, research, and testing of the theories and programs
+ * * to determine their effectiveness. The authors and publisher make * no
+ * warranty of any kind, expressed or implied, with regard to these * programs
+ * or to the documentation contained in these books. The authors * and publisher
+ * shall not be liable in any event for incidental or * consequential damages in
+ * connection with, or arising out of, the * furnishing, performance, or use of
+ * these programs. *
  *************************************************************************/
--- Tp2/Ejercicio3/src/Fig10_04_09/TestEmpleado.java
+++ Tp2/Ejercicio3/src/Fig10_04_09/TestEmpleado.java
@@ -5,26 +5,34 @@ import java.time.Month;
 
 public class TestEmpleado {
 	public static void main(String[] args) {
-		SalariedEmployee salariedEmployee = new SalariedEmployee( "John", "Smith", "111-11-1111", 800.00 );
-		HourlyEmployee hourlyEmployee = new HourlyEmployee( "Karen", "Price", "222-22-2222", 16.75, 40 );
-		CommissionEmployee commissionEmployee =new CommissionEmployee("Sue", "Jones", "333-33-3333", 10000, .06 );
-		BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, .04, 300 );
-	
-		salariedEmployee.setBirthDate(LocalDate.of(2006, Month.JANUARY, 4));
-		hourlyEmployee.setBirthDate(LocalDate.of(2006, Month.OCTOBER, 12));
-		commissionEmployee.setBirthDate(LocalDate.of(2003, Month.APRIL, 13));
-		basePlusCommissionEmployee.setBirthDate(LocalDate.of(1990, Month.AUGUST, 20));
+		//Creamos los empleados
+		SalariedEmployee empleadoSalariado = new SalariedEmployee("Jose", "Smith", "111-11-1111", 800.00);
+		HourlyEmployee empleadoPorHora = new HourlyEmployee("Wilder", "Price", "222-22-2222", 16.75, 40);
+		CommissionEmployee empleadoPorComision = new CommissionEmployee("Karen", "Jones", "333-33-3333", 10000, .06);
+		BasePlusCommissionEmployee empleadoMasBasePlusComision = new BasePlusCommissionEmployee("Bob", "Lewis",
+				"444-44-4444", 5000, .04, 300);
+
+		// Cargamos la fecha de compleaños de los empleados
+		empleadoSalariado.setBirthDate(LocalDate.of(2006, Month.JANUARY, 4));
+		empleadoPorHora.setBirthDate(LocalDate.of(2006, Month.OCTOBER, 12));
+		empleadoPorComision.setBirthDate(LocalDate.of(2003, Month.APRIL, 13));
+		empleadoMasBasePlusComision.setBirthDate(LocalDate.of(1990, Month.AUGUST, 20));
+
+		Employee[] empleados = { empleadoSalariado, empleadoPorHora, empleadoPorComision, empleadoMasBasePlusComision };
 		
-		Employee[] empleados = { salariedEmployee, hourlyEmployee, commissionEmployee,basePlusCommissionEmployee};
-		for(Employee e: empleados) {
+		LocalDate mesLiquidacion = LocalDate.of(2000, Month.JANUARY, 12);
+		LocalDate mesDistinto = LocalDate.of(2007, Month.DECEMBER, 1);
+		System.out.printf("Adicional en mes de cumpleaños: " + mesLiquidacion.getMonthValue()+"\n");
+		
+		for (Employee e : empleados) {
 			System.out.println(e.getFirstName());
-			System.out.printf("Mes de cumpleaños de: "+e.getBirthDate().getMonthValue());
+			System.out.print("Mes de cumpleaños: " + e.getBirthDate().getMonthValue()+"\n");
+			//Caso 1
+			System.out.println("Liquidacion en su mes de cumpleaños ("+mesLiquidacion+"): $"+ e.salary(mesLiquidacion));
 			
-			LocalDate mesLiquidacion = LocalDate.of(2000, Month.JANUARY, 12);
-			System.out.printf(" Liquidacion en mes de cumpleaños: "+ mesLiquidacion, e.salary(mesLiquidacion));
-			//System.out.println(" "+ e.earnings());
+			//Caso 2
+			System.out.println("Liquidacion fuera de su mes de cumpleaños ("+mesDistinto+"): $"+e.salary(mesDistinto));
 		}
-		
-		
+
 	}
 }
