ec9302ca4dfb4801790b6baa4f44e2c29c79e560
josenatteschoque <josenatteschoque@gmail.com>
2026-09-06 00:46:23 -0300

    Ejercicio Finalizado!

--- Tp3/Ejercicio5/src/carlosfontela/utilidades/PruebaNumerables.java
+++ Tp3/Ejercicio5/src/carlosfontela/utilidades/PruebaNumerables.java
@@ -8,6 +8,9 @@ public class PruebaNumerables {
 		Numerable ds1 = new DiaSemana(2);
 		Numerable ds2 = new DiaSemana(5);
 		
+		Numerable t1 = new Tiempo(15, 38, 45);
+		
+		
 		System.out.println (UtilidadesNumerables.mayor (dm1, ds1));
 		
 		System.out.println (UtilidadesNumerables.suma (dm1, ds1));
@@ -29,11 +32,21 @@ public class PruebaNumerables {
 		System.out.println(dm1.mostrar());
 		System.out.println(ds1.mostrar());
 
+		//Pruebo el metodo toInt de la clase Tiempo
+		System.out.println("Valor esperado = 56325 --> "+t1.toInt());
+
 		/*
 		System.out.println (v[0].toInt( ));
 		System.out.println (v[1].toInt( ));
 		System.out.println (v[2].toInt( ));
 		System.out.println (v[3].toInt( ));
 		*/
+
+		// prueba de validacion: esto deberia lanzar IllegalArgumentException
+		try {
+			Numerable tInvalido = new Tiempo(24, 0, 0);
+		} catch (IllegalArgumentException e) {
+			System.out.println("Se detecto correctamente un Tiempo invalido (hora 24).");
+		}
 	}
 }
--- Tp3/Ejercicio5/src/carlosfontela/utilidades/Tiempo.java
+++ Tp3/Ejercicio5/src/carlosfontela/utilidades/Tiempo.java
@@ -3,16 +3,47 @@ package carlosfontela.utilidades;
 public class Tiempo implements Numerable{
 	private int hora;
 	private int minuto;
+	private int segundos;
+	
+	
+	public Tiempo(int hora, int minuto, int segundos) {
+		super();
+		if(!valida(hora,minuto,segundos)) {
+			throw new IllegalArgumentException ();
+		}
+		this.hora = hora;		
+		this.minuto = minuto;
+		this.segundos = segundos;
+	}
+	
+	public boolean valida(int h, int m, int s) {
+		if(h < 0 || h > 23 ) {
+			return false;
+		}
+		
+		if(m < 0 || m > 59) {
+			return false;
+		}
+		
+		if(s < 0 || s > 59) {
+			return false;
+		}
+		
+		return true;
+	}
 	@Override
 	public int toInt() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.hora * 3600 + this.minuto * 60 + this.segundos;
+	}
+
+	@Override
+	public String toString() {
+		return "Tiempo [hora=" + hora + ", minuto=" + minuto + ", segundos=" + segundos + "]";
 	}
 
 	@Override
 	public String mostrar() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.toString();
 	}
 
 }
